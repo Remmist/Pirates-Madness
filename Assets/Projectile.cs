18e951d63a8329317eab989594ad32e56565d609
Juliusz <s23751@pjwstk.edu.pl>
2023-10-14 10:27:01 +0200

    Update Projectile.cs
    Added other.CompareTag condition, so the projectile destroys enemies.

--- Assets/Projectile.cs
+++ Assets/Projectile.cs
@@ -1,18 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _speed;
+    private Rigidbody2D _rb;
+    
+    private void Awake()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    // Start is called before the first frame update
+    private void Start()
     {
-        
+        _rb.velocity = transform.right * _speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Destroy(other.gameObject);
+        }
     }
 }
