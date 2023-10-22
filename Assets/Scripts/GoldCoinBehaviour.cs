cbeee8a3fd992b2f189131120de28a447737a3e9
Juliusz <s23751@pjwstk.edu.pl>
2023-10-22 14:40:40 +0200

    Create GoldCoinBehaviour.cs
    Behaviour for gold coins

---
+++ Assets/Scripts/GoldCoinBehaviour.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldenCoins : CollectibleItem
+{
+
+    private static int _goldCoinCounter;
+    
+    protected override void CollectBehaviour()
+    {
+        _goldCoinCounter++;
+        Debug.Log("You have collected " + _goldCoinCounter + " gold coins!");
+    }
+}
