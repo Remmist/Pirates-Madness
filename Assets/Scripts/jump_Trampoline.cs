cfde77d5773774d9dc9518ca4a04968b191cbc4b
Nadzeya Kokarava <127196631+1nKokoreva@users.noreply.github.com>
2023-11-12 23:27:51 +0100

    improving the trampoline code
    creating a radius and trigger for animation, edits in the animator

--- Assets/Scripts/jump_Trampoline.cs
+++ Assets/Scripts/jump_Trampoline.cs
@@ -2,15 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class jump : MonoBehaviour
+public class Trampoline : MonoBehaviour
 {
-    private float bounce = 25f;
+    [SerializeField] private float bounceForce = 25f;
+    [SerializeField] private float trampolineRadius = 2.5f; // Новая переменная для радиуса трамплина
 
+    private Animator trampolineAnimator;
+
+    private void Start()
+    {
+        // Получаем компонент Animator у трамплина
+        trampolineAnimator = GetComponent<Animator>();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce, ForceMode2D.Impulse);
+            Vector2 trampolineCenter = transform.position;
+            Vector2 playerPosition = collision.transform.position;
+
+            // Проверяем, находится ли игрок внутри радиуса трамплина
+            if (Vector2.Distance(playerPosition, trampolineCenter) <= trampolineRadius)
+            {
+                collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounceForce, ForceMode2D.Impulse);
+                
+                // Запускаем анимацию трамплина
+                trampolineAnimator.SetTrigger("BounceTrigger");
+            }
         }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        // Рисуем гизмос для отображения радиуса трамплина в редакторе
+        Gizmos.DrawWireSphere(transform.position, trampolineRadius);
     }
 }
