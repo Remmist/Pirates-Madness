3b270245062e12dc8be269285aae4ffe4f2f31b1
Ivan Illarionov <s25134@pjwstk.edu.pl>
2023-11-20 10:42:00 +0100

    New breakable wall WIP

--- Assets/Scripts/BreakableWall.cs
+++ Assets/Scripts/BreakableWall.cs
@@ -4,16 +4,26 @@ public class BreakableWall : MonoBehaviour
 {
     [SerializeField] private float _health;
     private float _currentHealth;
+    private bool _isAlive;
+
+    private Animator _animator;
 
     private void Awake()
     {
         _currentHealth = _health;
+        _animator = GetComponent<Animator>();
+        _isAlive = true;
     }
 
 
     public void TakeDamage(float damage)
     {
+        if (!_isAlive)
+        {
+            return;
+        }
         _currentHealth -= damage;
+        _animator.SetTrigger("Hit");
 
         if (_currentHealth <= 0)
         {
@@ -24,6 +34,7 @@ public class BreakableWall : MonoBehaviour
 
     private void DestroyWall()
     {
-        Destroy(gameObject);
+        _isAlive = false;
+        _animator.SetTrigger("Break");
     }
 }
